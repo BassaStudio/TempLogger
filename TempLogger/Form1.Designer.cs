a9f8d846dc3cefe2269c3c7d1088d59ad7542d33
Mads Bruun Jensen <madsj812@gmail.com>
2018-06-10 14:18:59 +0200

    adding project to git

---
+++ TempLogger/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TempLogger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Cont_text = new System.Windows.Forms.Button();
+            this.Ports_compo = new System.Windows.Forms.ComboBox();
+            this.tempchart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tempchart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Cont_text
+            // 
+            this.Cont_text.Location = new System.Drawing.Point(139, 22);
+            this.Cont_text.Name = "Cont_text";
+            this.Cont_text.Size = new System.Drawing.Size(75, 23);
+            this.Cont_text.TabIndex = 1;
+            this.Cont_text.Text = "connect";
+            this.Cont_text.UseVisualStyleBackColor = true;
+            this.Cont_text.Click += new System.EventHandler(this.Cont_text_Click);
+            // 
+            // Ports_compo
+            // 
+            this.Ports_compo.FormattingEnabled = true;
+            this.Ports_compo.Location = new System.Drawing.Point(12, 22);
+            this.Ports_compo.Name = "Ports_compo";
+            this.Ports_compo.Size = new System.Drawing.Size(121, 24);
+            this.Ports_compo.TabIndex = 2;
+            // 
+            // tempchart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.tempchart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.tempchart.Legends.Add(legend1);
+            this.tempchart.Location = new System.Drawing.Point(12, 52);
+            this.tempchart.Name = "tempchart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "TempSeries";
+            this.tempchart.Series.Add(series1);
+            this.tempchart.Size = new System.Drawing.Size(659, 434);
+            this.tempchart.TabIndex = 4;
+            this.tempchart.Text = "Temp";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(577, 463);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 498);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tempchart);
+            this.Controls.Add(this.Ports_compo);
+            this.Controls.Add(this.Cont_text);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tempchart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Cont_text;
+        private System.Windows.Forms.ComboBox Ports_compo;
+        private System.Windows.Forms.DataVisualization.Charting.Chart tempchart;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TempLogger/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TempLogger
+{
+    public partial class Form1 : Form
+    {
+        bool isConnect = false;
+        String[] ports;
+        SerialPort port;
+
+        string Temp = "12";
+
+        Thread listen;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        short getTemp()
+        {
+            Random r = new Random();
+            short.TryParse(r.Next(5, 24).ToString(), out short temp);
+            return temp;
+        }
+
+        void getAvailableComPorts()
+        {
+            ports = SerialPort.GetPortNames();
+
+            foreach (string port in ports)
+            {
+                Ports_compo.Items.Add(port);
+                if (ports[0] == null)
+                {
+                    Ports_compo.SelectedItem = ports[0];
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getAvailableComPorts();
+        }
+
+        private void dataListen()
+        {
+            while (isConnect /*port.IsOpen*/)
+            {
+                //label1.Text = port.ReadLine();
+            }
+        }
+
+        private void connectToArduino()
+        {
+            isConnect = true;
+
+            string selectedPort = Ports_compo.GetItemText(Ports_compo.SelectedItem);
+            port = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
+            port.Open();
+
+            Cont_text.Text = "Disconnect";
+
+            if (port.IsOpen)
+            {
+                listen = new Thread(dataListen);
+                listen.Start();
+            }
+        }
+
+        private void disconnectFromArduino()
+        {
+            isConnect = false;
+            port.Close();
+            Cont_text.Text = "Connect";
+        }
+
+        private void Cont_text_Click(object sender, EventArgs e)
+        {
+            if (!isConnect)
+            {
+                connectToArduino();
+            }
+            else
+            {
+                disconnectFromArduino();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+ 
+        }
+
+        short[] Data_chart; 
+
+        public void chart()
+        {
+            Data_chart = new short[8];
+
+            for (short i = 0; i < Data_chart.Length; i++)
+            {
+                Data_chart[i] = getTemp();
+            }
+
+            UpdateChart();
+        }
+
+        private void UpdateChart()
+        {
+            tempchart.Series["TempSeries"].Points.Clear();
+
+            for(int i = 0; i < Data_chart.Length - 1; i++)
+            {
+                tempchart.Series["TempSeries"].Points.AddY(Data_chart[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart();
+        }
+    }
+}
