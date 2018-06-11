d41bf9db4f4d1429eb6b855d51b613bdd3b5a990
Mads Jensen <31339803+BassaStudio@users.noreply.github.com>
2018-06-11 13:11:12 +0200

    Create serial.ino

---
+++ ardiono/testCode/serial.ino
@@ -0,0 +1,44 @@
+#include <LiquidCrystal.h>
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+String commandString = "";
+
+void setup() {
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(stringComplete) {
+    getCommand();
+    if(commandString == "LOW") {
+      
+    }
+  }
+}
+
+
+
+
+void getCommand()
+{
+  if(inputString.length()>0)
+  {
+     commandString = inputString.substring(3,6);
+  }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
